e4fadde765eb4a0d1613c1860a9552a58784251c
Gurparshad Singh <gurparshad0878@gmail.com>
2026-01-24 22:40:18 +0530

    Add files via upload

---
+++ max.py
@@ -0,0 +1,6 @@
+# Python Program to print maximum of 3 numbers
+num1 =int(input("enter 1st number\n"))
+num2 =int(input("enter 2nd number\n"))
+num3 =int(input("enter 3rd number\n"))
+maximum = max(num1,num2,num3)
+print(maximum)
