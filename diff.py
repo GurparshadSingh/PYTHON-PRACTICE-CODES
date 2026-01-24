30d69cdb6fd0a4120e91326e2bbd59ca62d247dd
Gurparshad Singh <gurparshad0878@gmail.com>
2026-01-24 22:04:51 +0530

    Add files via upload

---
+++ diff.py
@@ -0,0 +1,6 @@
+# Write a Python program to get the difference between a given number and 17, if the number is greater than 17 return double the absolute difference
+a=20
+if a<17:
+    print(a-17)
+else:
+    print(2*(a-17))
