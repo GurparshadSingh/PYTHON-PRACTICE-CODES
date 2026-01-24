d0d4454e8f84050cbdea17a691f9301ded672d97
Gurparshad Singh <gurparshad0878@gmail.com>
2026-01-24 22:45:43 +0530

    Add files via upload

---
+++ isLeap.py
@@ -0,0 +1,7 @@
+# Write a python program to find whether a given year is leap or not
+year =int(input("enter any year of ur choice"))
+
+if year%4==0 or year%400==0:
+    print("entered year is leap year")
+else:
+    print("entered year is not leap year")
