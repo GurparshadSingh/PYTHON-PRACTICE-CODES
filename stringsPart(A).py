ec51ac58e32ed355ec6579366c1c4839d4e87137
Gurparshad Singh <gurparshad0878@gmail.com>
2026-03-12 22:25:59 +0530

    Add files via upload

---
+++ stringsPart(A).py
@@ -0,0 +1,28 @@
+# Question__1
+# Write a Python program to find the length of a string entered by the user.
+# string = str(input("enter string to get length of it : "))
+# print(len(string))
+
+# Question__1
+# Write a Python program to print the first and last character of a string.
+# string = "Singh"
+# print("first letter is: ",string[0])
+# length=len(string)
+# print("first letter is: ",string[length-1])
+
+# Question__3
+# Write a Python program to reverse a string using slicing.
+# string = "Singh"
+# print(string[::-1])
+
+# Question__4
+# Write a Python program to print every second character of a string.
+# string = "SinghisKing"
+# print(string[::2])
+
+# Question__5
+# Write a Python program to check if a substring exists inside another string.
+# string = "SinghisKing"
+# str = "ghi"
+# if str in string:
+#     print("true")
