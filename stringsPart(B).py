37120239f7005077fc34f66d9c6ad040a6651abe
Gurparshad Singh <gurparshad0878@gmail.com>
2026-03-16 00:25:38 +0530

    Create stringsPart(B).py

---
+++ stringsPart(B).py
@@ -0,0 +1,40 @@
+# Write a Python program to convert a string to uppercase
+# name = str(input("enter ur name"))
+# print(name.upper())
+
+# Write a Python program to remove leading and trailing spaces from a string.
+# name = str(input("enter ur name"))
+# print(name.strip())
+
+# Write a Python program to replace a word in a sentence.
+# name = str(input("enter ur name"))
+# newStr = name.replace("guru","Singh")
+# print(newStr)
+
+# Write a Python program to count occurrences of a character in a string.
+# name = "abc"
+# count= name.count('m')
+# print(f"count of character 'a' character is {count}")
+
+# Write a Python program to count the number of vowels in a string
+# name="aafc"
+# count = 0
+# for character in name:
+#     if character in "aeiou":
+#         count+=1
+# print(count)        
+
+#  Write a Python program to remove duplicate characters from a string.
+# name = str(input("enter ur name"))
+# string = ""
+# for ch in name:
+#     if ch not in string:
+#         string+=ch
+# print(string)       
+
+#  Write a Python program to reverse the order of words in a sentence
+# sent = "i will master programming"
+# tukre = sent.split()   # split on spaces
+# newStr = " ".join(tukre[::-1])
+# print(newStr)
+
